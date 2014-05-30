1f8e2e0269ff06f7545513e654598a7582d2d63b
rrcemuzb <rrcemuzb@gmail.com>
2014-05-30 19:59:04 +0500

    30052014 Comment Dobavili

--- src/main/java/uz/micros/jstore/service/blog/PostService.java
+++ src/main/java/uz/micros/jstore/service/blog/PostService.java
@@ -3,7 +3,12 @@ package uz.micros.jstore.service.blog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uz.micros.jstore.entity.blog.Blog;
+import uz.micros.jstore.entity.blog.Comment;
 import uz.micros.jstore.entity.blog.Post;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class PostService {
@@ -16,8 +21,16 @@ public class PostService {
 
         Blog blog = blogSrv.getBlog();
 
-        for(Post post: blog.getPosts()){
-            if(post.getId()==id){
+        for (Post post : blog.getPosts()) {
+            if (post.getId() == id) {
+                Comment comment = new Comment();
+                comment.setDate(new Date());
+                comment.setAuthor("Davron");
+                comment.setText("Let's go ");
+
+                List<Comment> list = new ArrayList<Comment>();
+                list.add(comment);
+                post.setComments(list);
                 return post;
             }
         }
